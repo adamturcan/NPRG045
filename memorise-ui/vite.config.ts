12761f0af4061f3030884df01923ac68c543e434
Adam Turčan <adam.turcan@drake.sk>
2025-10-07 12:01:33 +0200

    add github deploy config

--- memorise-ui/vite.config.ts
+++ memorise-ui/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/~55063713/memorise-ui/",
+  base: "/NPRG045/",
   plugins: [react()],
   server: {
     proxy: {
